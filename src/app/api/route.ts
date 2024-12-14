66445128b43e0a265c2ea737964da314afcd698c
Tim <tf00185077@gmail.com>
2024-12-15 04:23:33 +0800

    add route

---
+++ src/app/api/route.ts
@@ -0,0 +1,6 @@
+'use server'
+import { NextResponse } from "next/server";
+
+export async function GET() {
+  return NextResponse.json({ message: "Hello World" });
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
-'use client'
+'use client';
 import Image from "next/image";
-import { getUser as getMongoUser } from "./getUser";
-import { useState } from "react";
+// import { getUser as getMongoUser } from "./getUser";
+// import { useState } from "react";
 const Home = () => {
-  const [name, setName] = useState('');
-
+  // const [name, setName] = useState('');
+  const getTest = async () => {
+    // 'use server'
+    const response = await fetch('http://localhost:3000/api');
+    const data = await response.json();
+    console.log(data);
+  };
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
@@ -18,7 +23,7 @@ const Home = () => {
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li>
-            <button onClick={getMongoUser}>GET USER DATA</button>
+            <button onClick={getTest}>GET USER DATA</button>
           </li>
           <li className="mb-2">
             test123 started by editing{" "}
@@ -105,5 +110,5 @@ const Home = () => {
       </footer>
     </div>
   );
-}
+};
 export default Home;
